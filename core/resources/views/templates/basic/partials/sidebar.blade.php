d22127f768139c415136bd9994b2afa1c869adb8
Mahe Karim <mahekarim@gmail.com>
2024-02-13 15:52:04 +0600

    Design Update :rocket:

--- core/resources/views/templates/basic/partials/sidebar.blade.php
+++ core/resources/views/templates/basic/partials/sidebar.blade.php
@@ -10,7 +10,7 @@
             </li>
 
             <li class="sidebar-item active">
-                <a class="sidebar-link" href="index.html">
+                <a class="sidebar-link" href="{{ route('user.home') }}">
                     <i class="align-middle" data-feather="sliders"></i> <span
                         class="align-middle">Dashboard</span>
                 </a>
@@ -95,16 +95,5 @@
             </li>
         </ul>
 
-        <div class="sidebar-cta">
-            <div class="sidebar-cta-content">
-                <strong class="d-inline-block mb-2">Upgrade to Pro</strong>
-                <div class="mb-3 text-sm">
-                    Are you looking for more components? Check out our premium version.
-                </div>
-                <div class="d-grid">
-                    <a href="upgrade-to-pro.html" class="btn btn-primary">Upgrade to Pro</a>
-                </div>
-            </div>
-        </div>
     </div>
 </nav>

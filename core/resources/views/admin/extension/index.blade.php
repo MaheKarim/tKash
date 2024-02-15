b482ea9b34a1082e33e0d338edc97cd9011bf951
Mahe Karim <mahekarim@gmail.com>
2024-02-15 14:52:48 +0600

    Bug Fixed :bug:

--- core/resources/views/admin/extension/index.blade.php
+++ core/resources/views/admin/extension/index.blade.php
@@ -19,7 +19,7 @@
                                 <tr>
                                     <td>
                                         <div class="user">
-                                            <div class="thumb"><img src="{{ asset('{{ getImage(getFilePath('extensions') .'/'. $extension->image,getFileSize('extensions')) }}" alt="{{ __($extension->name) }}" class="plugin_bg"></div>
+                                            <div class="thumb"><img src="{{ getImage(getFilePath('extensions') .'/'. $extension->image,getFileSize('extensions')) }}" alt="{{ __($extension->name) }}" class="plugin_bg"></div>
                                             <span class="name">{{ __($extension->name) }}</span>
                                         </div>
                                     </td>
